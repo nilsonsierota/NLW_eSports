a21e4fdb9ccfa95c695023d8e002a91681cbf6c1
Nilson Sierota <nilsoncv2@gmail.com>
2022-09-13 14:15:15 -0400

    Home Screen Commit

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import { FlatList, Image, View } from "react-native";
 
-import { View } from "react-native";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
 
 import { styles } from "./styles";
+import { GAMES } from "../../utils/games";
 
 export function Home() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre o seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      />
+    </View>
+  );
 }
