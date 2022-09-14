ab4b3af60fd55c719b211de56dcd32c34dc8b515
Nilson Sierota <nilsoncv2@gmail.com>
2022-09-14 10:55:29 -0400

    prisma start with schema sqlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,35 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
-app.get("/ads", (request, response) => {
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany();
+
+  return response.json(games);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: "Anuncio 1" },
     { id: 1, name: "Anuncio 2" },
     { id: 1, name: "Anuncio 3" },
   ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333, () => console.log("Server is Running at port 3333"));
